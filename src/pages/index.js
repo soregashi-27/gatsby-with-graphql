148fa1e2c27449e68ff5ee52dfe0b77768391d01
soregashi-27 <tomo.soul.15@gmail.com>
2021-06-14 23:23:51 +0900

    環境構築

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi there.</h1>
+    <p>This is a sample site "Gatsby with graphql" for study gatsbyjs.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
@@ -21,7 +21,6 @@ const IndexPage = () => (
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
 )
