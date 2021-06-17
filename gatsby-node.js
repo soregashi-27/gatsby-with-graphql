883e4eb0b5b0dcbef38749f404d2cd90e250acb3
soregashi-27 <tomo.soul.15@gmail.com>
2021-06-17 22:07:02 +0900

    メンタリング前の動作確認

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,62 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+/**
+ * ここで1ページあたりの投稿を2つ、特定の記事の詳細が見れるようにする
+ * pathを作る感じ。
+ *
+ * ※振り返り用にコメントを残している
+ */
+
+const path = require("path")
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  const postTemplate = path.resolve("src/templates/blog-post.js")
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+              date
+              author
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
+
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+      })
+    })
+  })
+}
+
+/**
+ * 「Promiseを使った表示制御について」
+ * エラーチェックの応答
+ * エラーが発生した場合は、戻る
+ * エラーがなければ問題なくデータを返す
+ * ループ処理をする
+ *
+ */
+
+// ここを書き換える時は、必ずterminalを止めてから再度スタートさせること
+
+// htmlとIDを加えた理由は？
+
+// graph qlのexpertって何者？
