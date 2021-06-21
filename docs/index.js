e544bbd010157c14c1df11f090af461d63538601
soregashi-27 <tomo.soul.15@gmail.com>
2021-06-22 08:27:01 +0900

    github pages用にdocsを追加

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Hi there.</h1>
+    <p>This is a sample site "Gatsby with graphql" for study gatsbyjs.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+  </Layout>
+)
+
+export default IndexPage
